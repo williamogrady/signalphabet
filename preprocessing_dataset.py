c2e271e95c77f4e57a50ff980b602542f6363129
Matilda Forslund <matforsl@kth.se>
2023-12-05 12:10:34 +0100

    small edits in preprocessing file
    Added comments and changed file path to dataset

---
+++ preprocessing_dataset.py
@@ -0,0 +1,59 @@
+import os
+import pickle
+
+import mediapipe as mp
+import cv2
+import matplotlib.pyplot as plt
+
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+# Saving directory path to our dataset in a variable
+DATA_DIR = '../asl_alphabet_train'  # TODO change to the right directory path for our dataset
+
+# Creating variables for the data and the corresponding labels
+data = []
+labels = []
+
+#TODO change to work with our dataset
+
+# looping through folders for the different letters
+# TODO change var.name dir_ to letter?
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
+
+        x_ = []
+        y_ = []
+
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(img_rgb)
+        # if a landmarks of a hand is found, it loops thorugh each point, 
+        # collecting and storing the x- and y- coordinates in lists
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+
+            data.append(data_aux)
+            labels.append(dir_)
+
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()

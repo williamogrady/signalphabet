5da80398f1f311e388566a60b0da14ab12437776
williamogrady <ogrady@kth.se>
2023-12-08 20:00:58 +0100

    billy hemmadator får cv2, scikit att funka
    wiho

--- interface/gui-import-webcam.py
+++ interface/gui-import-webcam.py
@@ -1,1 +1,17 @@
 import tkinter as tk
+import cv2
+import sklearn
+
+cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+
+frame_flipped = cv2.flip(frame, 1)  # flipping frames to display as mirrored
+frame_rgb = cv2.cvtColor(frame_flipped, cv2.COLOR_BGR2RGB)  # converting to rgb for usage with mediapipe 
+
+if True: 
+    cv2.imshow("frame", frame_flipped)
+    cv2.waitKey(25) 
+
+cap.release() 
+

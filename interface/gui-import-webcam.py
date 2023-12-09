8ea91094010e25bdcaa1d0e5a8b26182c166a42f
williamogrady <ogrady@kth.se>
2023-12-09 17:39:27 +0100

    Testar Webcam inuti tkinter
    Kopierad kod

--- interface/gui-import-webcam.py
+++ interface/gui-import-webcam.py
@@ -1,17 +1,54 @@
-import tkinter as tk
-import cv2
-import sklearn
+from tkinter import *
+import cv2 
+from PIL import Image, ImageTk 
+  
+vid = cv2.VideoCapture(0) # Define a video capture object 
+  
+width, height = 800, 600 # Declare the width and height in variables 
+  
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)  # Set the width and height 
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height) 
+  
 
-cap = cv2.VideoCapture(0)
+app = Tk() # Create a GUI app 
+  
 
-ret, frame = cap.read()
+app.bind('<Escape>', lambda e: app.quit())  # Bind the app with Escape keyboard to quit app whenever pressed 
+  
 
-frame_flipped = cv2.flip(frame, 1)  # flipping frames to display as mirrored
-frame_rgb = cv2.cvtColor(frame_flipped, cv2.COLOR_BGR2RGB)  # converting to rgb for usage with mediapipe 
-
-if True: 
-    cv2.imshow("frame", frame_flipped)
-    cv2.waitKey(25) 
-
-cap.release() 
-
+label_widget = Label(app) # Create a label and display it on app 
+label_widget.pack() 
+  
+# Create a function to open camera and 
+# display it in the label_widget on app 
+  
+  
+def open_camera(): 
+    # Capture the video frame by frame 
+    _, frame = vid.read() 
+  
+    # Convert image from one color space to other 
+    opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA) 
+  
+    # Capture the latest frame and transform to image 
+    captured_image = Image.fromarray(opencv_image) 
+  
+    # Convert captured image to photoimage 
+    photo_image = ImageTk.PhotoImage(image=captured_image) 
+  
+    # Displaying photoimage in the label 
+    label_widget.photo_image = photo_image 
+  
+    # Configure image in the label 
+    label_widget.configure(image=photo_image) 
+  
+    # Repeat the same process after every 10 seconds 
+    label_widget.after(10, open_camera) 
+  
+  
+# Create a button to open the camera in GUI app 
+button1 = Button(app, text="Open Camera", command=open_camera) 
+button1.pack() 
+  
+# Create an infinite loop for displaying app on screen 
+app.mainloop() 

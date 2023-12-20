b4471228372c49ae71cec32312d0b32ce8c10bb5
williamogrady <ogrady@kth.se>
2023-12-20 11:42:31 +0100

    Update gpt.py

--- interface/gpt.py
+++ interface/gpt.py
@@ -16,36 +16,34 @@ class App:
 
         self.frame1 = Frame(self.master)
         self.frame1.grid_rowconfigure(0, weight=1)
-        self.frame1.grid_columnconfigure(0, weight=7)
-        self.frame1.grid_columnconfigure(1, weight=1)
+        self.frame1.grid_columnconfigure(0, weight=4)
+        self.frame1.grid_columnconfigure(1, weight=4)
 
-        self.reg_txt = ttk.Label(self.frame1, text='login')
-        self.reg_txt.grid(row=0, column=0, sticky="w", pady=10)
-
-        self.practice_btn = ttk.Button(self.frame1, text="Go to Alphabet Page", command=self.alphabet_page)
-        self.practice_btn.grid(row=1, column=0, sticky="w", pady=10)
+        self.create_left_grid()
 
         self.label_widget = Label(self.frame1)
-        self.label_widget.grid(row=0, column=1, rowspan=2, padx=10, pady=10, sticky="nsew")
+        self.label_widget.grid(row=0, column=1, rowspan=3, padx=10, pady=10, sticky="nsew")
 
         self.vid = cv2.VideoCapture(0)
         self.show_frame()
 
         self.frame1.pack(expand=True, fill="both")
 
+    def create_left_grid(self):
+        for i in range(3):
+            self.frame1.grid_rowconfigure(i, weight=1)
+            for j in range(3):
+                frame_color = "red" if (i + j) % 2 == 0 else None
+                frame = Frame(self.frame1, bg=frame_color)
+                frame.grid(row=i, column=0, sticky="nsew")
+                frame.grid_columnconfigure(0, weight=1)
+
     def show_frame(self):
         ret, frame = self.vid.read()
 
         if ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-            # Get the dimensions of the label widget
-            label_width = self.label_widget.winfo_width()
-            label_height = self.label_widget.winfo_height()
-
-            # Crop and resize the frame to fill the label widget
-            frame = self.crop_and_resize(frame, label_width, label_height)
-
+            frame = self.crop_and_resize(frame, 300, 300)  # Adjust dimensions as needed
             img = Image.fromarray(frame)
             imgtk = ImageTk.PhotoImage(image=img)
 
@@ -80,10 +78,6 @@ class App:
         else:
             # Return an empty black frame if the cropping box is invalid
             return np.zeros((target_height, target_width, 3), dtype=np.uint8)
-
-    def alphabet_page(self):
-        # Add functionality for transitioning to the alphabet page
-        pass
 
 if __name__ == "__main__":
     root = Tk()

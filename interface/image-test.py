56546e9998275f2139c7606b72fa8eab9990a362
williamogrady <ogrady@kth.se>
2023-12-21 17:39:14 +0100

    Images Added, files and interface

--- interface/image-test.py
+++ interface/image-test.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
-import cv2
 from PIL import Image, ImageTk
+import os
 
 class Application(tk.Tk):
     def __init__(self):
@@ -10,45 +11,32 @@ class Application(tk.Tk):
         self.title("Sign Alphabet")
 
         self.current_page = None
+        self.selected_letter = None
 
-        self.camera_page()
-        self.start_camera()
+        #self.camera_page()
+        #self.start_camera()
         self.show_start_page()
 
-    def camera_page(self):
-        self.camera_frame = tk.Frame(self, width=600, height=900)
+    #def camera_page(self):
+        self.camera_frame = tk.Frame(self, width=640, height=480)
         self.camera_frame.pack(side=tk.LEFT, padx=10, pady=10)
 
         self.camera_label = tk.Label(self.camera_frame)
         self.camera_label.pack()
 
-    def start_camera(self):
-        self.cap = cv2.VideoCapture(0)
-        self.update_camera()
+    #def start_camera(self):
+        # Your camera initialization code goes here
+        pass
 
-
-    def update_camera(self):
-        ret, frame = self.cap.read()
-
-        if ret:
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = Image.fromarray(image)
-            image = image.resize((640, 480))
-            photo = ImageTk.PhotoImage(image)
-
-            self.camera_label.configure(image=photo)
-            self.camera_label.image = photo
-
-        self.after(10, self.update_camera)
-
-
-
+    #def update_camera(self):
+        # Your camera update code goes here
+        pass
 
     def show_start_page(self):
         if self.current_page:
             self.current_page.destroy()
 
-        self.current_page = tk.Frame(self, width=600, height=900)
+        self.current_page = tk.Frame(self, width=200, height=480)
         self.current_page.pack(padx=10, pady=300)
 
         label = tk.Label(self.current_page, text="SignAlphabet", font=("FOT-RodinNTLG Pro DB", 48))
@@ -63,13 +51,11 @@ class Application(tk.Tk):
         quit_button = tk.Button(self.current_page, text="Quit", bg="lightcoral", command=self.confirm_quit)
         quit_button.grid(pady=20)
 
-
-
     def show_alphabet_page(self):
         if self.current_page:
             self.current_page.destroy()
 
-        self.current_page = tk.Frame(self, width=600, height=900)
+        self.current_page = tk.Frame(self, width=200, height=480)
         self.current_page.pack(padx=10, pady=300)
 
         label = tk.Label(self.current_page, text="Pick a letter!", font=("FOT-RodinNTLG Pro DB", 16))
@@ -88,30 +74,40 @@ class Application(tk.Tk):
         go_back_button = tk.Button(self.current_page, text="Go Back", bg="lightgreen", command=self.show_start_page)
         go_back_button.grid(row=7, column=0, columnspan=5, pady=(10, 0))
 
-
-
     def show_practice_page(self, selected_letter):
         if self.current_page:
             self.current_page.destroy()
 
-        self.current_page = tk.Frame(self, width=600, height=900)
+        self.current_page = tk.Frame(self, width=200, height=480)
         self.current_page.pack(padx=10, pady=300)
 
-        image_path = f"interface/images/{selected_letter}.png"
-        img = Image.open(image_path)
-        img = img.resize((200, 200))  
-        photo = ImageTk.PhotoImage(img)
-
-        image_label = tk.Label(self.current_page, image=photo)
-        image_label.photo = photo
-        image_label.grid(row=1, column=0, columnspan=5, padx=10, pady=10)
+        self.selected_letter = selected_letter
 
         label_text = f"You're learning to sign '{selected_letter}'"
         label = tk.Label(self.current_page, text=label_text, font=("FOT-RodinNTLG Pro DB", 16))
         label.grid(row=0, column=0, columnspan=5, pady=(0, 10))
 
+        text = tk.Label(app, text="Do this")
+        text.grid(row=0, column=1)
+
+        image = Image.open("interface/testpicture.png").resize((400, 400))
+        bing = ImageTk.PhotoImage(image)
+
+        label = ttk.Label(app, image=bing)
+        label.grid(row=1, column=1)
+
+        # Load and display the image
+        image_path = f"interface/images/{selected_letter}.png"
+        img = Image.open(image_path)
+        img = img.resize((200, 200))  # Adjust the size as needed
+        photo = ImageTk.PhotoImage(img)
+
+        image_label = tk.Label(self.current_page, image=photo)
+        image_label.photo = photo
+        image_label.grid(row=1, column=0, columnspan=5, pady=(10, 0))
+
         go_back_button = tk.Button(self.current_page, text="Go Back", bg="lightgreen", command=self.show_alphabet_page)
-        go_back_button.grid(row=1, column=0, columnspan=5, pady=(200, 0))
+        go_back_button.grid(row=2, column=0, columnspan=5, pady=(10, 0))
 
     def confirm_quit(self):
         result = messagebox.askyesno("Quit", "Are you sure you want to quit?")
